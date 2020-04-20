2bb750147b869345cb8138f76bcd3716628eecb7
ibrahimshamma99 <i.abushammah@gmail.com>
2020-04-20 06:35:23 +0300

    http error handled

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
               'content-type': 'multipart/form-data'
           }
       };
-      axios.post("localhost:5000/upload",formData,config)
+      axios.post("http://localhost:5000/upload",formData,config)
           .then((response) => {
               alert("The file is successfully uploaded");
           }).catch((error) => {
@@ -37,6 +37,7 @@ class App extends React.Component {
           <form onSubmit={this.onFormSubmit}>
               <h1>File Upload</h1>
               <input type="file" className="custom-file-input" name="myImage" onChange= {this.onChange} />
+              {console.log(this.state.file)}
               <button className="upload-button" type="submit">Upload to DB</button>
           </form>
       )
